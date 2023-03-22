1c9599d6f334559028c83720c6bf0c16d56ea9b4
User <rakhimzhanov.na05@gmail.com>
2023-03-22 14:35:04 +0600

    Added registration

--- hw.py
+++ hw.py
@@ -4,10 +4,53 @@ from data import db_session
 from flask import Flask, render_template, redirect
 from data.users import User
 from data.jobs import Jobs
+from forms.user import RegisterForm
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/')
+def work_log():
+    db_session.global_init("db/mars_explorer.db")
+    db_sess = db_session.create_session()
+    jobs = [job for job in db_sess.query(Jobs).all()]
+    return render_template('work_log.html', jobs=jobs)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            surname=form.surname.data,
+            name=form.name.data,
+            email=form.email.data,
+            age=int(form.age.data),
+            address=form.address.data,
+            specialty=form.specialty.data,
+            position=form.position.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login')
+def login():
+    return "Ura!"
 
 
 def main():
